62bf6d235c91c2c4ba5f6f27fd1d5eb1457b7e07
UmangSardesai <umang2610@gmail.com>
2015-03-04 15:34:51 +0530

    First Commit

---
+++ examples/conf1.js
@@ -0,0 +1,25 @@
+// An example configuration file.
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+  specs: ['example_spec.js'],
+
+//Capabilities to be passed to the webdriver instance.
+  capabilities: 
+  {
+    'browserName' : 'Safari',
+    'browser_version' : '8.0',
+    'os': 'OS X',
+    'os_version': 'Yosemite',
+    'resolution': '1024x768',
+    'build':'Sample Protractor Tests',
+    'browserstack.user': '',
+    'browserstack.key': ''
+  },
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
---
+++ examples/conf2.js
@@ -0,0 +1,66 @@
+// An example configuration file.
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+  specs: ['example_spec.js'],
+
+multiCapabilities: [
+{
+    'browserName' : 'Chrome',
+    'browser_version' : '36.0',
+    'os': 'Windows',
+    'os_version': '8.1',
+    'resolution': '1024x768',
+    'build':'Sample Protractor Tests',
+    'browserstack.debug' : 'true',
+    'browserstack.user': '',
+    'browserstack.key': ''
+},
+{
+    'browserName' : 'Safari',
+    'browser_version' : '8.0',
+    'os': 'OS X',
+    'os_version': 'Yosemite',
+    'resolution': '1024x768',
+    'build':'Sample Protractor Tests',
+    'browserstack.debug' : 'true',
+    'browserstack.user': '',
+    'browserstack.key': ''
+},
+{
+    'browserName' : 'Firefox',
+    'browser_version' : '30.0',
+    'os': 'Windows',
+    'os_version': '7',
+    'resolution': '1024x768',
+    'build':'Sample Protractor Tests',
+    'browserstack.debug' : 'true',
+    'browserstack.user': '',
+    'browserstack.key': ''
+},
+{
+    'browserName' : 'android',
+    'platform' : 'ANDROID',
+    'device': 'Samsung Galaxy S5',
+    'build':'Sample Protractor Tests',
+    'browserstack.debug' : 'true',
+    'browserstack.user': '',
+    'browserstack.key': ''
+},
+{
+    'browserName' : 'iPhone',
+    'platform' : 'MAC',
+    'device': 'iPhone 5S',
+    'build':'Sample Protractor Tests',
+    'browserstack.debug' : 'true',
+    'browserstack.user': '',
+    'browserstack.key': ''
+}
+],
+
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000
+  }
+};
