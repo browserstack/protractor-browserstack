d9e398b945d395221c99b067e52efc54a67789b1
tayal1996 <tayalm08@gmail.com>
2021-07-23 18:44:46 +0530

    fix: mobile and desktop compatible script

--- specs/single.js
+++ specs/single.js
@@ -1,9 +1,15 @@
-describe('Google\'s Search Functionality', function() {
-  it('can find search results', function() {
-    browser.driver.get('https://google.com/ncr').then(function() {
-      browser.driver.findElement(by.name('q')).sendKeys('BrowserStack\n').then(function() {
-        expect(browser.driver.getTitle()).toEqual('BrowserStack - Google Search');
-      });
-    });
+const { browser } = require("protractor");
+
+describe("Google's Search Functionality", function () {
+  it("can find search results", async function () {
+    await browser.driver.get("https://google.com/ncr");
+    await browser.driver.findElement(by.name("q")).sendKeys("BrowserStack", protractor.Key.ENTER); // this submits on desktop browsers
+    try {
+      await browser.wait(protractor.ExpectedConditions.titleContains("Search"), 1000);
+    } catch (e) {
+      await browser.driver.findElement(by.name("q")).submit(); // this helps in mobile browsers
+    }
+    await browser.wait(protractor.ExpectedConditions.titleContains("Search"), 1000);
+    await expect(browser.driver.getTitle()).toMatch(/BrowserStack/i);
   });
 });
