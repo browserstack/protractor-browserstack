b5954130227e8d98aaa02001eb319f6cdac83cd1
tayal1996 <tayalm08@gmail.com>
2021-07-23 19:56:08 +0530

    fix: matched wait with standard bs tests

--- specs/single.js
+++ specs/single.js
@@ -1,13 +1,20 @@
+const { browser, ExpectedConditions } = require("protractor");
+
 describe("Google's Search Functionality", function () {
   it("can find search results", async function () {
     await browser.driver.get("https://google.com/ncr");
-    await browser.driver.findElement(by.name("q")).sendKeys("BrowserStack", protractor.Key.ENTER); // this submits on desktop browsers
+    const searchBox = await browser.driver.findElement(by.name("q"));
+    await searchBox.sendKeys("BrowserStack", protractor.Key.ENTER); // this submits on desktop browsers
     try {
-      await browser.wait(protractor.ExpectedConditions.titleContains("Search"), 1000);
+      await browser.wait(async () => {
+        return /BrowserStack/i.test(await browser.driver.getTitle());
+      }, 1000);
     } catch (e) {
-      await browser.driver.findElement(by.name("q")).submit(); // this helps in mobile browsers
+      await searchBox.submit(); // this helps in mobile browsers
     }
-    await browser.wait(protractor.ExpectedConditions.titleContains("Search"), 1000);
-    await expect(browser.driver.getTitle()).toMatch(/BrowserStack/i);
+    await browser.wait(async () => {
+      return /BrowserStack/i.test(await browser.driver.getTitle());
+    }, 1000);
+    expect(await browser.driver.getTitle()).toMatch(/BrowserStack/i);
   });
 });
